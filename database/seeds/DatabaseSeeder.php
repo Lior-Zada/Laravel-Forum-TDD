25acda4be9dbd33a852b0405a20e7e38f2882038
Lior-Zada <lior305@gmail.com>
2020-05-26 14:13:42 +0300

    added to Database seeder to auto create a test user and reset Redis cache, and mark 3 threads as trending

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Trending;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +18,30 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         $threads = factory('App\Thread', 50)->create();
 
-        $threads->each(function($thread){
+        $threads->each(function ($thread) {
             factory('App\Reply', 10)->create(['thread_id' => $thread->id]);
         });
+
+        // create my test user
+        create('App\User', [
+            'name' => 'LiorZada',
+            'email' => 'lior305@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123123123'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        $trending = new Trending();
+
+        //reset redis cached data
+        $trending->reset();
+        // set first 3 created threads as trending
+        collect($threads)
+            ->filter(function ($value, $key) {
+                return $key < 3;
+            })
+            ->each(function ($thread) use ($trending) {
+                $trending->push($thread);
+            });
     }
 }
